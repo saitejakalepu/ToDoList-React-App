38fbfb72a1dfd89a2b6204a0f2d1ff4e8e44458e
saitejababu <saiteja.kalepu@gmail.com>
2020-08-20 19:17:33 +0530

    Updates

---
+++ precache-manifest.3479fa1b54961dabc5d2ab4d32b4a7b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd8cc626de7208bc54e862f667a64d3",
+    "url": "/ToDoList-React-App/index.html"
+  },
+  {
+    "revision": "1383ae534b186c6049d5",
+    "url": "/ToDoList-React-App/static/css/main.edbc5656.chunk.css"
+  },
+  {
+    "revision": "d2de44eabc09b6db5b68",
+    "url": "/ToDoList-React-App/static/js/2.2b95304d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoList-React-App/static/js/2.2b95304d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1383ae534b186c6049d5",
+    "url": "/ToDoList-React-App/static/js/main.4930e744.chunk.js"
+  },
+  {
+    "revision": "3aa855ffc599558f2f9a",
+    "url": "/ToDoList-React-App/static/js/runtime-main.f10af3a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ToDoList-React-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
